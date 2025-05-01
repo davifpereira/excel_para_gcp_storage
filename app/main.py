d541646e927eb1c1add26f0ffabe0a221c04a854
Davi <davifpereira@yahoo.com.br>
2025-05-01 15:12:38 -0300

    build: criação das duas funções destinadas à leitura e ao tratamento de dados dos arquivos excel. Função de tratamento já contém a validação do contrato de dados do Pandera

--- app/main.py
+++ app/main.py
@@ -2,18 +2,32 @@ import os
 
 import pandas as pd
 from dotenv import load_dotenv
+from pandera.errors import SchemaError
+from schema import SchemaAvaliacaoForn, SchemaClassifCredito
+
+# Variáveis gerais
 
 load_dotenv()
 
+# Arquivo do setor Financeiro: Classificação de crédito dos clientes
 caminho_classif_credito = os.getenv("CAMINHO_EXCEL_CLASSIF_CREDITO_CLIENTES")
+nome_tabela_classif_credito = "excel_classificacao_credito_"
+col_base_classif_credito = ["ID_CLIENTE", "NOME_PESSOA"]
+col_valores_classif_credito = ["Analista 1", "Analista 2", "Gerente", "Score Externo"]
+
+# Arquivo de Suprimentos e Qualidade: Avaliação de fornecedores
 caminho_avaliacao_forn = os.getenv("CAMINHO_EXCEL_AVALIACAO_FORNECEDORES")
+nome_tabela_avaliacao_forn = "excel_avaliacao_fornecedores_"
+
+
+# Funções
 
 
 def LerArquivo(caminho: str, aba: str, coluna_dados: str) -> pd.DataFrame:
     """
     Função destinada a leitura primária do arquivo Excel,
-    cujo objetivo é retornar um dicionario contendo
-    alguns metadados e um dataframe com os dados tabulares
+    cujo objetivo é retornar um dicionario contendo alguns metadados e
+    um dataframe com os dados tabulares
     """
 
     df = pd.read_excel(caminho, sheet_name=aba, header=None)
@@ -26,22 +40,25 @@ def LerArquivo(caminho: str, aba: str, coluna_dados: str) -> pd.DataFrame:
         coluna_dados: "DADOS",
     }
 
+    # Pesquisa coluna a coluna para encontrar em qual está as palavras chaves do dicionario acima
     for nome_col, col_chave in metadados.items():
 
         for col in df.columns:
 
+            # Verifica se a palavra chave está presente na coluna iterada
             check_metadados = df[col].astype(str).str.contains(nome_col, na=False)
 
+            # caso sim, guarda o número da coluna e da linha
             if check_metadados.any():
 
                 coluna = col + 1
                 linha = df[check_metadados].index[0]
 
+                # Guarda num dicionário a data de avaliação, o revisor e os dados de avaliação
                 if nome_col == coluna_dados:
 
-                    df_selecionado = df.iloc[linha:, col:]
-                    df_selecionado.columns = df_selecionado.iloc[0]
-                    df_selecionado = df_selecionado[1:].reset_index(drop=True)
+                    # Dados de avaliação
+                    df_selecionado = df.iloc[linha - 1 :, col:].reset_index(drop=True)
 
                     conteudo_planilha[col_chave] = df_selecionado
 
@@ -49,6 +66,7 @@ def LerArquivo(caminho: str, aba: str, coluna_dados: str) -> pd.DataFrame:
 
                     resultado = df.iloc[linha, coluna]
 
+                    # Data de avaliação e Revisor
                     if col_chave == "DATA_AVALIACAO":
 
                         resultado = pd.to_datetime(resultado, format="%d/%m/%Y").date()
@@ -57,7 +75,127 @@ def LerArquivo(caminho: str, aba: str, coluna_dados: str) -> pd.DataFrame:
 
                 break
 
+        # Retém ainda o caminho do arquivo
+        conteudo_planilha["CAMINHO"] = caminho
+
     return conteudo_planilha
 
 
-conteudo = LerArquivo(caminho_classif_credito, "Dados", "ID do Cliente")
+def TratarDataframe(
+    conteudo: dict, colunas_base: list, colunas_valores: list
+) -> pd.DataFrame:
+    """
+    É resposável pelo tratamento dos dados com base nestas ações:
+
+    - Elabora um dataframe para concetrar o peso percentual da avaliação de cada agente;
+    - Delimita o dataframe principal (aquele que contém os dados das avaliações, propriamente);
+    - Transforma as colunas de agentes e suas avaliações em linhas (unpivot);
+    - Realiza o merge do dataframe principal com o de pesos percentuais;
+    - Inclui duas colunas no dataframe: data de avaliação e revisor.
+    """
+    tabela = {}
+
+    # O objetivo desse diconário é
+    caminho_raiz_nome_arquivos = {
+        caminho_classif_credito: nome_tabela_classif_credito,
+        caminho_avaliacao_forn: nome_tabela_avaliacao_forn,
+    }
+
+    # Dicionário que será destinado à alteração do nome das colunas
+    altera_nome_colunas = {
+        "ID do Cliente": "ID_CLIENTE",
+        "Nome da Pessoa": "NOME_PESSOA",
+    }
+
+    # Seleciona os dados de avaliação,
+    # para iniciar a separação dos critérios percentuais e
+    # das avaliações, propriamente
+    df = conteudo["DADOS"]
+
+    # Dados dos critérios percentuais
+    df_criterios = df.iloc[0:2]
+
+    # Realiza essa iteração para excluir os dados nulos
+    for col in df_criterios.columns:
+
+        check_criterios = (
+            df_criterios[col].astype(str).str.contains("Critérios", na=False)
+        )
+
+        if check_criterios.any():
+
+            coluna = col + 1
+
+            df_criterios = df.iloc[0:2, coluna:]
+            df_criterios.columns = df_criterios.iloc[1]
+            df_criterios = df_criterios.iloc[0:1].reset_index(drop=True)
+
+            break
+
+    df_criterios = pd.melt(
+        frame=df_criterios,
+        value_vars=df_criterios.columns,
+        var_name="AGENTE",
+        value_name="PESO_PERCENTUAL",
+    )
+
+    # Tabela dos dados de avaliação
+    df.columns = df.iloc[1]
+    df_tabela_dados = df[2:].reset_index(drop=True)
+
+    df_tabela_dados = df_tabela_dados.rename(columns=altera_nome_colunas)
+
+    df_tabela_dados = pd.melt(
+        frame=df_tabela_dados,
+        id_vars=colunas_base,
+        value_vars=colunas_valores,
+        var_name="AGENTE",
+        value_name="SCORE",
+    )
+
+    # Junta a tabela dos dados de avaliação com a que contém
+    # o peso percentual da avaliação de cada agente
+    df_tabela_dados = df_tabela_dados.merge(right=df_criterios, on="AGENTE")
+
+    # Incluir uma coluna para a data de avaliação
+    data_avaliacao = conteudo["DATA_AVALIACAO"]
+    df_tabela_dados["DATA_AVALIACAO"] = data_avaliacao
+
+    # Inclui uma coluna para o revisor da avaliação
+    revisor = conteudo["REVISOR"]
+    df_tabela_dados["REVISOR"] = revisor
+
+    # Define o nome que a tabela terá no data warehouse
+    caminho_arquivo = conteudo["CAMINHO"]
+    raiz_nome_arquivo = caminho_raiz_nome_arquivos[caminho_arquivo]
+    nome_arquivo = raiz_nome_arquivo + data_avaliacao.strftime("%Y_%m_%d")
+
+    # Valida os contratos de dados
+    try:
+        if raiz_nome_arquivo == nome_tabela_classif_credito:
+
+            SchemaClassifCredito.validate(df_tabela_dados)
+
+        elif raiz_nome_arquivo == nome_tabela_avaliacao_forn:
+
+            SchemaAvaliacaoForn.validate(df_tabela_dados)
+
+    except SchemaError as e:
+        print(f"Falha na validação do esquema: {e}")
+
+    tabela[nome_arquivo] = df_tabela_dados
+
+    return tabela
+
+
+if __name__ == "__main__":
+
+    conteudo_classif_credito = LerArquivo(
+        caminho=caminho_classif_credito, aba="Dados", coluna_dados="ID do Cliente"
+    )
+
+    df_classificacao_credito = TratarDataframe(
+        conteudo=conteudo_classif_credito,
+        colunas_base=col_base_classif_credito,
+        colunas_valores=col_valores_classif_credito,
+    )
