74ded1c5c59a5b81add3399e7ca326329cbd605d
Davi Fernando Pereira <davifpereira@yahoo.com.br>
2025-05-10 23:20:32 -0300

    build: criação e teste da primeira dag do projeto, relativa ao arquivo de classificação de crédito

--- dags/main.py
+++ dags/main.py
@@ -1,16 +1,18 @@
 import os
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
 from datetime import datetime
 
 from airflow.decorators import dag, task
-from dotenv import load_dotenv
 
 from app.etl import CarregarDadosBigQuery, LerArquivo, TratarDataframe
 
-id_conjunto_dados_bigquery = os.getenv("ID_CONJUNTO_DADOS_BIGQUERY")
+load_dotenv()
 
 ##### Variáveis gerais
-
-load_dotenv()
 
 # Arquivo do setor Financeiro: Classificação de crédito dos clientes
 caminho_classif_credito = os.getenv("CAMINHO_EXCEL_CLASSIF_CREDITO_CLIENTES")
@@ -23,11 +25,51 @@ caminho_avaliacao_forn = os.getenv("CAMINHO_EXCEL_AVALIACAO_FORNECEDORES")
 
 nome_tabela_avaliacao_forn = "excel_avaliacao_fornecedores_"
 
-
-id_conjunto_dados_bigquery = os.getenv("ID_CONJUNTO_DADOS_BIGQUERY")
-
 # O objetivo deste dicionário é criar o nome da tabela que será salva no big query
 caminho_raiz_nome_arquivos = {
     caminho_classif_credito: nome_tabela_classif_credito,
     caminho_avaliacao_forn: nome_tabela_avaliacao_forn,
 }
+
+##### DAGs
+
+
+@dag(
+    dag_id="processar_arquivo_classifi_credito",
+    description="Responsável pelo processamento do arquivo financeiro Classificação de Crédito.xlsx",
+    schedule="0 10 * * *",  # Executa diariamente às 07h (UTC -3)
+    start_date=datetime(2025, 5, 7),
+    catchup=False,
+)
+def ExecutarPipelineClassifCredito():
+
+    @task
+    def extract():
+        conteudo_planilha = LerArquivo(
+            caminho=caminho_classif_credito, aba="Dados", coluna_dados="ID do Cliente"
+        )
+
+        return conteudo_planilha
+
+    @task
+    def transform(conteudo_planilha):
+        nome_arquivo, df_tabela_dados = TratarDataframe(
+            conteudo=conteudo_planilha,
+            colunas_base=col_base_classif_credito,
+            colunas_valores=col_valores_classif_credito,
+            nome_tabela=caminho_raiz_nome_arquivos,
+        )
+
+        return df_tabela_dados, nome_arquivo
+
+    @task
+    def load(resultado_transformacao):
+        dataframe, nome_arquivo = resultado_transformacao
+        CarregarDadosBigQuery(dataframe=dataframe, nome_tabela=nome_arquivo)
+
+    dados = extract()
+    resultado = transform(dados)
+    load(resultado)
+
+
+ExecutarPipelineClassifCredito()
